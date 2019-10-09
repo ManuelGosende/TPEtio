1c300f0d9aed7a0a38d0aa5522e2d604e142fc35
ManuelGosende <mgosende5@gmail.com>
2019-10-09 17:57:20 -0300

    Avance y solución de issues en nueva rama

--- back.php
+++ back.php
@@ -5,5 +5,15 @@ class inmobiliarias{
     function __construct(){
 
     }
+    define("BASE_URL", 'http://'.$_SERVER["SERVER_NAME"].':'.$_SERVER["SERVER_PORT"].dirname($_SERVER["PHP_SELF"]).'/');
+    define("LOGIN", BASE_URL . 'login');
+    define("VER", BASE_URL . 'ver');
+    
+    public function getAll() {
+        $query = $this->db->prepare('SELECT * FROM tareas ORDER BY finalizada ASC');
+        $query->execute();
+
+        return $query->fetchAll(PDO::FETCH_OBJ);
+    }
 }
 
